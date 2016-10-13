653ed37af8065b49f24813b6962f0f0dbd7e0cfc
Rachid <rachid@actived.fr>
2016-10-13 15:10:50 +0200

    Filtering activité list + manager create rubrique + select masked filters

--- site/templates/rubrique.php
+++ site/templates/rubrique.php
@@ -1,7 +1,45 @@
 <?php include '_header.php';?>
 
-<div class="">
-    <select></select>
-</div>
+<!-- List of filters -->
+<form method="GET">
+    <?php
+    $filters = $pages->find('parent=1030');
+    $allowedFilters = [];
+    foreach($filters as $f){
+        //If filter is masked, do not display it
+        if($page->rub_masked_filters->has($f)){
+            continue;
+        }
+
+        //Add filter to allowed codes
+        $allowedFilters[] = $f->code;
+
+        //Display the select
+        echo "<div class='form-group col-xs-2'><label>{$f->title}</label><select class='form-control' name='{$f->code}'><option></option>";
+            foreach($f->children() as $child){
+                echo "<option value='{$child->id}' ".($input->get[$f->code] == $child->id ? 'SELECTED':'').">{$child->title}</option>";
+            }
+        echo "</select></div>";
+    }
+    ?>
+    <div class="form-group col-xs-2">
+        <label>&nbsp;</label>
+        <input class="btn btn-success form-control" type="submit" value="Filtrer"/>
+    </div>
+</form>
+
+<!-- -->
+<?php
+$params = array_intersect_key(array_filter($_GET), array_flip($allowedFilters));
+$list = $page->children($params);
+?>
+
+<!-- Results -->
+<div class="clearfix"></div>
+<?php foreach($list as $item):?>
+    <h3>
+        <?=$item->title?>
+    </h3>
+<?php endforeach;?>
 
 <?php include '_footer.php';?>
